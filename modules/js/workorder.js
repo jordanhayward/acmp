a48f2e6662bc9d84ccb2858a8966ac3134db66e0
Lee Grey <lee.grey@kony.com>
2014-05-22 19:12:41 -0400

    Mostly finished with Visualizer "recovery" with the exception of the Location/Asset hierarchy navigator.

--- modules/js/workorder.js
+++ modules/js/workorder.js
@@ -4,6 +4,26 @@ function toggleATF() {
 	// TODO show/hide widgets
 }
 
+// work order filtering UI //////////////////////////////////////////////////////////////////////////////
+
+function setLocationFilter() {
+	// TODO update filter display
+	// TODO filter segment
+	popLocationFilter.dismiss()
+}
+
+function setPriorityFilter() {
+	// TODO update filter display
+	// TODO filter segment
+	popPriorityFilter.dismiss()
+}
+
+function setDueDateFilter() {
+	// TODO update filter display
+	// TODO filter segment
+	popDueDateFilter.dismiss()
+}
+
 // work order CRUD //////////////////////////////////////////////////////////////////////////////
 
 function createWO() {
@@ -14,6 +34,13 @@ function createWO() {
 function updateWO() {
 	// TODO update WO
 	frmActiveWOList.show()
+}
+
+function sendMoreInfo() {
+	// TODO send more info for WO
+	// TODO remove more info from list
+	// TODO update more info badge to reflect reduction
+	frmMoreInfoList.show()
 }
 
 
